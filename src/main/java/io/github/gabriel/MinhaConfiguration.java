dcc98c729aac95b7dbb021a1bf96d69982504b9d
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-02-11 06:24:49 -0300

    criando arquivo de configuration e primeiro bean

---
+++ src/main/java/io/github/gabriel/MinhaConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.gabriel;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class MinhaConfiguration {
+
+    @Bean
+    public String applicationName() {
+        return "Sistema de vendas";
+    }
+
+}
