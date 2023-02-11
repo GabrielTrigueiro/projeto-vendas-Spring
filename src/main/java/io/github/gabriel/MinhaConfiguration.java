8979eeefc42d005b563f91e77754da30affac904
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-02-11 06:38:11 -0300

    config básica injetando uma string de uma classe em outra

--- src/main/java/io/github/gabriel/MinhaConfiguration.java
+++ src/main/java/io/github/gabriel/MinhaConfiguration.java
@@ -2,11 +2,12 @@ package io.github.gabriel;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class MinhaConfiguration {
 
-    @Bean
-    public String applicationName() {
+    @Bean(name = "applicationName")
+    public String applicationName(){
         return "Sistema de vendas";
     }
 
