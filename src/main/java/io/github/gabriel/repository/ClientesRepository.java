3046dd1ad095f0cb28136d33bb3d3267ddd119e0
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-02-11 10:39:35 -0300

    injetando depedências

---
+++ src/main/java/io/github/gabriel/repository/ClientesRepository.java
@@ -0,0 +1,12 @@
+package io.github.gabriel.repository;
+
+import io.github.gabriel.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+
+    public void persist(Cliente cliente) {
+        //acessa a base e salva o cliente
+    }
+}
--- src/main/java/io/github/gabriel/repository/MeuRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.gabriel.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class MeuRepository {
-}
